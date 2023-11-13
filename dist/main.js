4bf2e62fd6d6e9de594ccd346402608dddcb80f7
Yuusuf Roble <yuuusuf98@outlook.com>
2023-11-13 16:14:13 +0100

    i have added hard code everything on html and css now its javascript and dom manipulation time

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+alet('gasa');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQSxhIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdHVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiXG5hbGV0KCdnYXNhJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
